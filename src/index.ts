8408e29436e8c5790f4fbd5d91b8531ea61d9866
Yuyu <ujunglim@naver.com>
2021-06-05 20:37:10 +0800

    add class

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
 
-// create interface
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+  constructor(name:string, age:number, gender:string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "yujung",
-  age: 23,
-  gender: "female"
-}
+const stephen = new Human("Stephen", 15, "male");
 
 const sayHi = (person:Human):string => {
   return `Hello! ${person.name}. you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(stephen));
 
 export {};
